18de81bdaaca39a3b4e0269bacc28a329ee4949f
Pratibha <pratibhamishra794@gmail.com>
2018-11-25 12:28:02 +0530

    file added

---
+++ WeekDetailCell.h
@@ -0,0 +1,13 @@
+//
+//  WeekDetailCell.h
+//  FoodFuels
+//
+//  Created by Pratibha MbPro on 06/01/18.
+//  Copyright © 2018 Ezdat Technology Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeekDetailCell : UICollectionViewCell
+
+@end
